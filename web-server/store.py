6c62ce3fbc58f44e9c30c4806ce7e9d20c6e4112
Fernando Burgos <burgosfernando720@gmail.com>
2023-05-22 12:41:21 -0300

    corriguiendo erros

--- web-server/store.py
+++ web-server/store.py
@@ -4,7 +4,7 @@ def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
     print(r.status_code)
     print(r.text)
-    print(type(r))
+    print(type(r.text))
     elementos = r.job()
     for ele in elementos:
         print(ele["name"])
